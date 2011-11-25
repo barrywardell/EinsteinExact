e89e7b6a368efd21c9bd67b8454b73becaf661d1
Barry Wardell <barry.wardell@gmail.com>
2011-11-25 14:57:55 +0000

    Add copyright and readme.

--- EinsteinExact.m
+++ EinsteinExact.m
@@ -1,6 +1,21 @@
 (* ::Package:: *)
 
-(* An exact solutions thorn *)
+(* EinsteinExact: An exact solutions thorn 
+   Copyright 2011 Barry Wardell and Ian Hinder
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
 
 Get["KrancThorn`"];
 Needs["Metrics`"];
