86a616fb112dd9d822a936be5fc1602557703200
Samuel Cupp <scupp1@my.apsu.edu>
2018-02-27 11:05:24 -0600

    Vaidya2 changed to use PreSync

--- Vaidya2/src/Boundaries.cc
+++ Vaidya2/src/Boundaries.cc
@@ -2,6 +2,7 @@
 
 #include "cctk.h"
 #include "cctk_Arguments.h"
+#include "cctk_Arguments_Vaidya2.h"
 #include "cctk_Parameters.h"
 #include "cctk_Faces.h"
 #include "util_Table.h"
@@ -19,7 +20,7 @@
 
 extern "C" void Vaidya2_CheckBoundaries(CCTK_ARGUMENTS)
 {
-  DECLARE_CCTK_ARGUMENTS;
+  DECLARE_CCTK_ARGUMENTS_Vaidya2_CheckBoundaries;
   DECLARE_CCTK_PARAMETERS;
   
   return;
@@ -27,7 +28,7 @@ extern "C" void Vaidya2_CheckBoundaries(CCTK_ARGUMENTS)
 
 extern "C" void Vaidya2_SelectBoundConds(CCTK_ARGUMENTS)
 {
-  DECLARE_CCTK_ARGUMENTS;
+  DECLARE_CCTK_ARGUMENTS_Vaidya2_SelectBoundConds;
   DECLARE_CCTK_PARAMETERS;
   
   CCTK_INT ierr CCTK_ATTRIBUTE_UNUSED = 0;
