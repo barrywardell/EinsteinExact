9cb6ac77d2470a38fbbd0efabae96a24e5dfcabb
Roland Haas <rhaas@illinois.edu>
2020-05-11 07:56:19 -0500

    EinsteinExact/Minkowski: remove explicit include of cctk_Arguments_Checked

--- Minkowski/src/Boundaries.cc
+++ Minkowski/src/Boundaries.cc
@@ -1,7 +1,6 @@
 /*  File produced by Kranc */
 
 #include "cctk.h"
-#include "cctk_Arguments_Checked.h"
 #include "cctk_Parameters.h"
 #include "cctk_Faces.h"
 #include "util_Table.h"
--- Minkowski/src/RegisterSymmetries.cc
+++ Minkowski/src/RegisterSymmetries.cc
@@ -2,7 +2,6 @@
 
 #include "cctk.h"
 #include "cctk_Arguments.h"
-#include "cctk_Arguments_Checked.h"
 #include "cctk_Parameters.h"
 #include "Symmetry.h"
 
